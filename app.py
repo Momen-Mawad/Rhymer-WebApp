562bdada9bf3d68909731a40a95793eb300b919a
Momen-Mawad <Momen-Mawad@users.noreply.github.com>
2020-11-26 22:34:09 +0100

    Updated the data

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, request
 #from connect import df
 import pronouncing, pandas as pd
 
-df = pd.read_csv("static/data.csv")
+df = pd.read_csv("static/data.csv").sort_values(by='syllables')
+
 
 app = Flask(__name__)
 
