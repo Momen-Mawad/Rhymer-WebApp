828d7406f06d82f4d16ea8c2446c9e2913902be0
Momen-Mawad <Momen-Mawad@users.noreply.github.com>
2020-11-28 12:03:15 +0100

    Created a page that updates its self

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 #from connect import df
 import pronouncing, pandas as pd
 
@@ -7,15 +7,18 @@ df = pd.read_csv("static/data.csv").sort_values(by='syllables')
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
-def upload_file():
+def main():
     if request.method == 'POST':
         word = request.form.get('Rhyme-Word')
+        x = int(request.form.to_dict().get('syllables-num'))
         dfFiltered = df.loc[df['text'].str.split().str[-1].isin(pronouncing.rhymes(word))]
-        return render_template('main.html', file=dfFiltered.iterrows())
+        dfFiltered = dfFiltered.loc[df['syllables'] == x]
+        return render_template('result.html', file=dfFiltered.iterrows(), number=str(x), word=word)
 
     return render_template('main.html')
 
-if __name__== "__main__":
+if __name__ == "__main__":
     app.secret_key = '123456'
     app.run(debug='Enable', use_reloader=True)
