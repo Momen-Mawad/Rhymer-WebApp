6deb612d2ab67a4f4259679b335d9bb96b6d7f3b
Momen-Mawad <Momen-Mawad@users.noreply.github.com>
2020-12-05 02:36:02 +0100

    Added checbkoxes and reset button with javascript

--- app.py
+++ app.py
@@ -2,22 +2,38 @@ from flask import Flask, render_template, request, url_for, redirect
 #from connect import df
 import pronouncing, pandas as pd
 
-df = pd.read_csv("static/data.csv").sort_values(by='syllables')
-
-
+df = pd.read_csv("static/data.csv").sort_values(by='syllables').reset_index(drop=True)
+df['usage'] = False
+x = df[:100]
+list(df.index)
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if request.method == 'POST':
+        global df
+        df2=df
         word = request.form.get('Rhyme-Word')
-        x = int(request.form.to_dict().get('syllables-num'))
-        dfFiltered = df.loc[df['text'].str.split().str[-1].isin(pronouncing.rhymes(word))]
-        dfFiltered = dfFiltered.loc[df['syllables'] == x]
-        return render_template('result.html', file=dfFiltered.iterrows(), number=str(x), word=word)
+        numberSyllable = int(request.form.to_dict().get('syllables-num'))
+        checklist = request.form.getlist('checklisthtml')
+        checklist = [int(x) for x in checklist]
+        for i in list(df2.index):
+            if i in checklist:
+                df.at[i, 'usage'] = True
+            
+        dfFiltered = df2.loc[df2['text'].str.split().str[-1].isin(pronouncing.rhymes(word))]
+        dfJSON = dfFiltered.to_json(orient='index')
+        return render_template('result.html', file=dfFiltered.iterrows(), df=df2,
+                               number=str(numberSyllable), x=checklist, dfJSON=dfJSON, word=word)
 
     return render_template('main.html')
+
+@app.route('/reset', methods=['GET', 'POST'])
+def reset():
+    if request.method == 'POST':
+        df['usage'] = False
+    return redirect(url_for('main'))
+
 
 if __name__ == "__main__":
     app.secret_key = '123456'
